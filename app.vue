5386dd608ec2f99d55c75baef313d34aed636cd3
alyilmaz99 <aliyilmaz9977@gmail.com>
2022-11-14 04:35:51 +0300

    add components

--- app.vue
+++ app.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <!-- <home-welcome/> -->
+  <button @click="toogle">Toogle</button>
+ <component :is='MyComponent'/>
+
+ 
+  <!-- bu şekilde de çağırabiliriz 
+    <Counter/>
+  <component :is='Counter'/> 
+   şeklinde de çağırabiliriz
+  -->
+  <!--
+      iki farklı şekilde de kullanılabliyor,
+     <HomeWelcome/>
+     <Counter/>
+  -->
 </template>
+
+<script setup>
+  const MyComponent = shallowRef(resolveComponent('Counter'));
+
+  function toogle(){
+    console.log(MyComponent.value)
+    MyComponent.value = resolveComponent('HomeWelcome');
+  }
+</script>
+
+<!-- İKİ FARKLI KULLANIMI VAR -->
+<!--
+<script>
+  export default{
+    data(){
+      return{
+        count: 0,
+      };
+    },
+    methods: {
+      increment(){
+        this.count++;
+      },
+      decrement(){
+        this.count--;
+      },
+    },  
+  };
+
+
+</script>
+-->
